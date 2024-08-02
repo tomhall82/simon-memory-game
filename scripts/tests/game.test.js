dc48e374ef04517376e825277f67514e984ee274
tomhall82 <tom.hall82@yahoo.co.uk>
2024-08-02 13:45:36 +0100

    intial game oject tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 
-const { beforeAll } = require("@jest/globals");
+const { game } = require("../game");
+
+// const { beforeAll } = require("@jest/globals");
 
 beforeAll(() => {
   let fs = require("fs");
@@ -10,4 +12,22 @@ beforeAll(() => {
   document.open();
   document.write(fileContents);
   document.close();
+});
+
+describe("game object contains correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true);
+  });
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+  test("choices contains correct ids", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
 });
