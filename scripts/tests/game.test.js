7cd29ede702c78413aa581abf65517b79e41f4ac
tomhall82 <tom.hall82@yahoo.co.uk>
2024-08-02 13:25:02 +0100

    beforeAll function added  to test HTML

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,13 @@
 /**
  * @jest-environment jsdom
  */
+
+const { beforeAll } = require("@jest/globals");
+
+beforeAll(() => {
+  let fs = require("fs");
+  let fileContents = fs.readFileSync("index.html", "utf-8");
+  document.open();
+  document.write(fileContents);
+  document.close();
+});
