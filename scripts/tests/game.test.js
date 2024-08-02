c8b9a1919fcf1f4bee9bf1ef3d71cd54e735fa85
tomhall82 <tom.hall82@yahoo.co.uk>
2024-08-02 15:01:52 +0100

    test lightsOn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,10 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { beforeAll } = require("@jest/globals");
-const { game, newGame, showScore, addTurn } = require("../game");
-
-// const { beforeAll } = require("@jest/globals");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 beforeAll(() => {
   let fs = require("fs");
@@ -52,5 +49,28 @@ describe("newGame works correctly", () => {
   });
   test("should display 0 for the element with the id of score", () => {
     expect(document.getElementById("score").innerText).toEqual(0);
+  });
+});
+
+describe("gameplay works correctly", () => {
+  beforeEach(() => {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    addTurn();
+  });
+  afterEach(() => {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+  });
+  test("addTurn adds a new turn to the game", () => {
+    addTurn();
+    expect(game.currentGame.length).toBe(2);
+  });
+  test("should add correct class to light up buttons", () => {
+    let button = document.getElementById(game.currentGame[0]);
+    lightsOn(game.currentGame[0]);
+    expect(button.classList).toContain("light");
   });
 });
