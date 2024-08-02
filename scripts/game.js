a50b47a12e13da06eec23943388d039cd9dc4d71
tomhall82 <tom.hall82@yahoo.co.uk>
2024-08-02 15:17:43 +0100

    build showTurn function

--- scripts/game.js
+++ scripts/game.js
@@ -16,7 +16,7 @@ function newGame() {
 function addTurn() {
   game.playerMoves = [];
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
-  //   showTurns();
+  showTurns();
 }
 
 function showScore() {
@@ -30,4 +30,15 @@ function lightsOn(circ) {
   }, 400);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+  game.turnNumber = 0;
+  let turns = setInterval(() => {
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
+  }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
