56cd3545b7c507b9d028c0c61871eb55cf5b9c52
tomhall82 <tom.hall82@yahoo.co.uk>
2024-08-02 17:14:29 +0100

    playerTurn function built and tested

--- scripts/game.js
+++ scripts/game.js
@@ -53,4 +53,26 @@ function showTurns() {
   }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function playerTurn() {
+  let i = game.playerMoves.length - 1;
+  if (game.currentGame[i] === game.playerMoves[i]) {
+    if (game.currentGame.length == game.playerMoves.length) {
+      game.score++;
+      showScore();
+      addTurn();
+    }
+  } else {
+    alert("Wrong move!");
+    newGame();
+  }
+}
+
+module.exports = {
+  game,
+  newGame,
+  showScore,
+  addTurn,
+  lightsOn,
+  showTurns,
+  playerTurn,
+};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const { expect } = require("@jest/globals");
 const {
   game,
   newGame,
@@ -9,7 +10,10 @@ const {
   addTurn,
   lightsOn,
   showTurns,
+  playerTurn,
 } = require("../game");
+
+jest.spyOn(window, "alert").mockImplementation(() => {});
 
 beforeAll(() => {
   let fs = require("fs");
@@ -94,5 +98,15 @@ describe("gameplay works correctly", () => {
     game.turnNumber = 42;
     showTurns();
     expect(game.turnNumber).toBe(0);
+  });
+  test("should increment score if the turn is correct", () => {
+    game.playerMoves.push(game.currentGame[0]);
+    playerTurn();
+    expect(game.score).toEqual(1);
+  });
+  test("should call an alert if the move is wrong", () => {
+    game.playerMoves.push("wrong");
+    playerTurn();
+    expect(window.alert).toBeCalledWith("Wrong move!");
   });
 });
